930672e59417eb293360a0a29a5ec875e08711c2
harold205043 <156283189+harold205043@users.noreply.github.com>
2024-06-15 17:23:25 -0400

    Add files via upload

---
+++ How2_style.js
@@ -0,0 +1,10 @@
+// Open the side navigation
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  // Close the side navigation
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
